98a7f4c9e54d6db9e5864c2d0364c3431f35aea6
bloodykot <kirdined@rambler.ru>
2023-09-24 23:15:40 +0300

    выполненные задания по спринту

---
+++ ImageFeed/ServiceAccessLayer/Network service/URLRequest.swift
@@ -0,0 +1,70 @@
+import Foundation
+
+// MARK: - HTTP Request
+extension URLRequest {
+    static func makeHTTPRequest(
+        path: String,
+        httpMethod: String,
+        baseURL: URL = URL(string: Constants.defaultAPIBaseURLString)!
+    ) -> URLRequest {
+        var request = URLRequest(url: URL(string: path, relativeTo: baseURL)!)
+        request.httpMethod = httpMethod
+        if let token = OAuth2TokenStorage.shared.token {
+            request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        }
+        return request
+    }
+}
+
+// MARK: - Network Connection
+enum NetworkError: Error {
+    case decodingError(Error)
+    case httpStatusCode(Int)
+    case urlRequestError(Error)
+    case urlSessionError
+    case invalidRequest
+}
+
+extension URLSession {
+    func objectTask<T: Decodable>(
+        for request: URLRequest,
+        completion: @escaping (Result<T, Error>) -> Void) -> URLSessionTask {
+            let decoder = JSONDecoder()
+            let urlSession = URLSession.shared
+            return urlSession.data(for: request) { (result: Result<Data, Error>) in
+                let response = result.flatMap { data -> Result<T, Error> in
+                    Result {try decoder.decode(T.self, from: data)}
+                }
+                completion(response)
+            }
+        }
+    
+    func data(
+        for request: URLRequest,
+        completion: @escaping (Result<Data, Error>) -> Void) -> URLSessionTask {
+        let fulfillCompletionOnMainThread: (Result<Data, Error>) -> Void = { result in
+            DispatchQueue.main.async {
+                completion(result)
+            }
+        }
+        let task = dataTask(with: request, completionHandler: { data, response, error in
+            if let data = data,
+               let response = response,
+               let statusCode = (response as? HTTPURLResponse)?.statusCode
+            {
+                if 200..<300 ~= statusCode {
+                    fulfillCompletionOnMainThread(.success(data))
+                } else {
+                    fulfillCompletionOnMainThread(.failure(NetworkError.httpStatusCode(statusCode)))
+                }
+            } else if let error = error {
+                fulfillCompletionOnMainThread(.failure(NetworkError.urlRequestError(error)))
+            } else {
+                fulfillCompletionOnMainThread(.failure(NetworkError.urlSessionError))
+            }
+        })
+        task.resume()
+        return task
+    }
+}
+
